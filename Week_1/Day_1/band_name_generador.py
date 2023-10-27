147fcd2db43514708de8483ec96c22d4a2f19222
allan-gadelha <c.allan.gadelha@gmail.com>
2023-10-27 16:22:12 -0300

    Created the first code and next folder

---
+++ Week_1/Day_1/band_name_generador.py
@@ -0,0 +1,13 @@
+#Band Name Generation
+
+#Program introduction
+print("Olá, vamos criar um nome para a sua banda!")
+
+#Asking birth city and saving in a variable
+cidade = input("Qual o nome da cidade que você nasceu?\n")
+
+#Asking pet name and saving in a variable
+pet = input("Qual o nome do seu pet?\n")
+
+#Printing the band name!
+print("O nome de sua Banda é: " + cidade + " " + pet )
