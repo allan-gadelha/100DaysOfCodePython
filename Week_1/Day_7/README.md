1e5e02937f39c3e98ebe2988966b0c4a6ca307be
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-11-01 14:09:16 -0300

    Code and Read update

--- Week_1/Day_7/README.md
+++ Week_1/Day_7/README.md
@@ -7,3 +7,5 @@ Part 2: Added Display so the player can see the if the choose exist in the chose
 Part 2.1: Changed the way the code gonna loop trought the word.
 
 Part 3: While loop to make the game running till the player gets the word right also conditional to see if the world is right, that probably will need to be changed in the future!
+
+Part 4: ASCII art, Lives, Win and Lose conditionals added.
--- Week_1/Day_7/hangman_game.py
+++ Week_1/Day_7/hangman_game.py
@@ -1,24 +1,84 @@
 import random
-#Step 1 
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+#Variable for controlling the End of Game trough Game Over or Win Condition
+end_of_game = False
+#Variable of Lives the player has
+lives = 6
+#List of Possible Words
 word_list = ["aardvark", "baboon", "camel"]
 
 #Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
+
 #This one so we can get the length of the chosen_word, it'll help to make the code cleaner
 word_length = len(chosen_word)
 
+print(f"Solution: {chosen_word}.")
+
+#Creating Blanks for the players to see what's missing
 display = []
-for letter in chosen_word:
-    display.append("_")
+for _ in range(word_length):
+    display += "_"
 
-#Debugging code
-print("chosen_word: ", chosen_word)
-#print(display)
-
-#Trying to create a while loop to keep the game running till there's no more "_" in the display
-while "_" in display:
-    print(display)
+#Loop to keep runing the game till end of game
+while not end_of_game:
     #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
     guess = input("Guess a letter: ").lower()
 
@@ -30,7 +90,22 @@ while "_" in display:
         if letter == guess:
             #Chance de display "_" to the letter guessed in the right position
             display[position] = letter
-            if "_" not in display:
-                print("You win!")
-                print(display)
-                break
+    
+    #if in case the player choose the wrong letter, live goes -1 and if it reach 0, its game over!
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+              #Print the final live, hanged man! 
+              print(f"{stages[lives]}")
+              end_of_game = True
+              print("You Lose!!")
+
+    #Print the Stage / Live you're in
+    print(f"{stages[lives]}")
+    #join all elements in the list and turn it into a String
+    print(f"{' '.join(display)}")
+
+    #Check to see if user got all the letters right! 
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
