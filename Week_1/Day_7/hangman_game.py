d450041400fbbd0862bf3f9bde4cf8a1d9d99b44
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-11-01 15:12:39 -0300

    Modules: ASCII art and word list. Also made QOL

--- Week_1/Day_7/hangman_game.py
+++ Week_1/Day_7/hangman_game.py
@@ -1,68 +1,16 @@
 import random
+#Importing modules!
+from hangman_art import logo, stages
+from hangman_words import word_list
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+#Print LOGO
+(print(logo))
 
 #Variable for controlling the End of Game trough Game Over or Win Condition
 end_of_game = False
+
 #Variable of Lives the player has
 lives = 6
-#List of Possible Words
-word_list = ["aardvark", "baboon", "camel"]
 
 #Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
@@ -70,7 +18,8 @@ chosen_word = random.choice(word_list)
 #This one so we can get the length of the chosen_word, it'll help to make the code cleaner
 word_length = len(chosen_word)
 
-print(f"Solution: {chosen_word}.")
+#Debug Option
+#print(f"Solution: {chosen_word}.")
 
 #Creating Blanks for the players to see what's missing
 display = []
@@ -81,6 +30,9 @@ for _ in range(word_length):
 while not end_of_game:
     #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
     guess = input("Guess a letter: ").lower()
+    #If the player already guessed that letter, just a feedback
+    if guess in display:
+        print(f"You've already guessed {guess}")
 
     #Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
     #Loop through each position in the chosen_word
@@ -93,12 +45,14 @@ while not end_of_game:
     
     #if in case the player choose the wrong letter, live goes -1 and if it reach 0, its game over!
     if guess not in chosen_word:
+        #Feedback for the player:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
         lives -= 1
         if lives == 0:
-              #Print the final live, hanged man! 
-              print(f"{stages[lives]}")
+              #End the game
               end_of_game = True
               print("You Lose!!")
+              print(f"The word was: {chosen_word}.")
 
     #Print the Stage / Live you're in
     print(f"{stages[lives]}")
