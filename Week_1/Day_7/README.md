79a4ddf4bcb107f383e7578b6eedfd1d487d09a8
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-11-01 07:18:06 -0300

    Update Readme and Code

--- Week_1/Day_7/README.md
+++ Week_1/Day_7/README.md
@@ -1,1 +1,5 @@
 Today is the last day of the first week and it's just the final project and i need to use everything i learned on it. The project is a Hangman Game
+
+Part 1: Added the basic loop for choosing the word from a list then getting input from player and checking if the input letter exist in the chosen word
+
+Part 2: Added Display so the player can see the if the choose exist in the chosen word or not
--- Week_1/Day_7/hangman_game.py
+++ Week_1/Day_7/hangman_game.py
@@ -6,14 +6,27 @@ word_list = ["aardvark", "baboon", "camel"]
 #Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 display = []
-print("chosen_word: ", chosen_word)
+for letter in chosen_word:
+    display.append("_")
+
+#Debugging code
+#print("chosen_word: ", chosen_word)
+#print(display)
 
 #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
 
 #Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+#Creating a variable for controlling the index of display
+i = 0
 for letter in chosen_word:
     if letter == guess:
         print("Right")
+        display[i] = letter
+        i += 1
     else:
         print("Wrong")
+        i += 1
+
+#Just to check the display
+print(display)
