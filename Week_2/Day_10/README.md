8c9baf08e84845713a0113dd6a8c0f03fd345d5f
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-11-04 16:42:10 -0300

    Calculator done and readme update

--- Week_2/Day_10/README.md
+++ Week_2/Day_10/README.md
@@ -1,3 +1,5 @@
 Today we learned about Functions with Outputs, Multiple return values, Combining Dictionaris and Functions, more about While loops, flags and Recursion.
 
 The project of this day is to make a calculator!
+
+The cool part about this day was learning about dict and saving functions in it, a really nice way to call functions using key from dict, i can see lots of useful ways to use it, but i still need more practice with it, still take me a time to understand and to create it from 0 if it gets too much complex!
--- Week_2/Day_10/calculator.py
+++ Week_2/Day_10/calculator.py
@@ -16,24 +16,59 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     """This function divide the first number by the second number"""
+    if n2 == 0:
+        return "Error: Division by zero"
+    else:
+        return n1 / n2
 
-def calculation(n1, n2, operation_order):
-    if operation_order == "+":
-        return n1 + n2
-    elif operation_order == "-":
-        return n1 - n2
-    elif operation_order == "*":
-        return n1-n2
+#Creating a recursion to make the program flow better
+def calculator():
 
-first_number = int(input("What's the first number?: "))
+    #Asking the user for the first number
+    first_number = float(input("What's the first number?: "))
 
-operation = input("+\n-\n*\n/\nPick a operation: ")
+    #Dictionarie to save operation with respective function, really nice way to save functions for a keyword!
+    operations = {
+        "+" : add,
+        "-" : subtract,
+        "*" : multiply,
+        "/" : divide
+    }
 
-second_number = int(input("What's the next number?: "))
+    #Printing operation for user to see the possibles ones
+    for symbol in operations:
+        print(symbol)
 
-operations = {
-    "+" : add,
-    "-" : subtract,
-    "*" : multiply,
-    "/" : divide
-}
+    #Loop to keep going till the user don't want anymore
+    should_continue = True
+    while should_continue:
+        
+        #Picking the operation in each iteration
+        operation_symbol = input("Pick a operation: ")
+
+        #If operation does not exist in operations give a feedback for the user, and make him try again!
+        if operation_symbol not in operations:
+            print("Invalid Operation")
+        else:
+            #If operation exist keep going and ask about the second number
+            second_number = float(input("What's the second number?: "))
+
+            #Creating a new variable that will get the function from the operations! So calculation_function becomes the function!!
+            calculation_function = operations[operation_symbol]
+
+            #Creating the answer!
+            answer = calculation_function(first_number, second_number)
+
+            #Printing the complete calculation
+            print(f"{first_number} {operation_symbol} {second_number} = {answer}")
+
+            #Asking if the user still wants to keep calculating
+            keep_calculating = input(f"Type 'y' to continue calculation with {answer}, or type 'n' to start a new calculation: ")
+            
+            if keep_calculating == "y":
+                first_number = answer
+            elif keep_calculating == "n":
+                should_continue = False
+                calculator()
+
+calculator()
