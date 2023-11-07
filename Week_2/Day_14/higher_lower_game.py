414717ae1d437bc4a4dc27f329e3c13e475c9405
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-11-07 06:57:52 -0300

    Game code completed

--- Week_2/Day_14/higher_lower_game.py
+++ Week_2/Day_14/higher_lower_game.py
@@ -7,18 +7,45 @@ print(logo)
 score = 0
 compare_a = []
 compare_b = []
+continue_game = True
 
-def comparing():
-    #Getting a random item from data
-    compare_a = data[random.randint(0,49)]
+def check_input():
+    """Function to check for a valid input from player"""
+    while True:
+        answer = input(f"Who has more followers? Type 'A' or 'B': ").lower()
+        if answer in ['a', 'b']:
+            return answer
+        print("Invalid input. Please type 'A' or 'B'.")
+
+
+compare_a = data[random.randint(0,49)]
+
+while continue_game:
     print(f"Compare A: {compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}")
+    print(f"Psst A has {compare_a['follower_count']}")
 
     print(vs)
 
-    #Getting a random item from data
     compare_b = data[random.randint(0,49)]
     print(f"Compare B: {compare_b['name']}, a {compare_b['description']}, from {compare_b['country']}")
+    print(f"Psst B has {compare_b['follower_count']}")
 
+    answer = check_input()
 
+    while compare_a == compare_b:
+        compare_b = data[random.randint(0,49)]
 
-comparing()
+    if answer == 'a':
+        if compare_a['follower_count'] > compare_b['follower_count']:
+            score += 1
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            continue_game = False
+
+    if answer == 'b':
+        if compare_b['follower_count'] > compare_a['follower_count']:
+            score += 1
+            compare_a = compare_b
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}")
+            continue_game = False
